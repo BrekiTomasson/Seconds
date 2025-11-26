cefe13109e0b84889d58d35c39cb4801fba77207
Breki <breki.tomasson@gmail.com>
2025-11-26 14:50:19 +0100

    Fixed typo in deprecation notice.

--- src/Seconds.php
+++ src/Seconds.php
@@ -14,25 +14,25 @@ enum Seconds: int
 
     case ONE_WEEK = 604_800;
 
-    /** @deprecated v1.1.2 and replaced with fromDays(int $days), will be removed in v2.0.0. */
+    /** @deprecated v1.2.0 and replaced with fromDays(int $days), will be removed in v2.0.0. */
     public static function days(int $days): int
     {
         return self::fromDays($days);
     }
 
-    /** @deprecated v1.1.2 and replaced with fromHours(int $hours), will be removed in v2.0.0. */
+    /** @deprecated v1.2.0 and replaced with fromHours(int $hours), will be removed in v2.0.0. */
     public static function hours(int $hours): int
     {
         return self::fromHours($hours);
     }
 
-    /** @deprecated v1.1.2 and replaced with fromMinutes(int $minutes), will be removed in v2.0.0. */
+    /** @deprecated v1.2.0 and replaced with fromMinutes(int $minutes), will be removed in v2.0.0. */
     public static function minutes(int $minutes): int
     {
         return self::fromMinutes($minutes);
     }
 
-    /** @deprecated v1.1.2 and replaced with fromWeeks(int $weeks), will be removed in v2.0.0. */
+    /** @deprecated v1.2.0 and replaced with fromWeeks(int $weeks), will be removed in v2.0.0. */
     public static function weeks(int $weeks): int
     {
         return self::fromWeeks($weeks);
